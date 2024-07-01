477d7dd12c7c4fff0ebd323fb50a40a8fca8934a
scheck93 <109469032+scheck93@users.noreply.github.com>
2024-07-01 23:55:35 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -2,10 +2,31 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  document
+    .getElementById("generateDomain")
+    .addEventListener("click", displayResult);
 };
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+function getRandomElement(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function generateRandomString(arr1, arr2, arr3) {
+  let part1 = getRandomElement(arr1);
+  let part2 = getRandomElement(arr2);
+  let part3 = getRandomElement(arr3);
+
+  return part1 + "" + part2 + "" + part3 + ".com";
+}
+
+function displayResult() {
+  let domain = generateRandomString(pronoun, adj, noun);
+  document.getElementById("theDomain").innerHTML = domain;
+}
